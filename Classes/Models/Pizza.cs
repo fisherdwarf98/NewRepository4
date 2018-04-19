d1116ff32d720b459e8c0726b8596d269839efe0
fisherdward98 <38538809+fisherdward98@users.noreply.github.com>
2018-04-19 14:13:09 -0500

    Class Practice

---
+++ Classes/Models/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classes.Models
+{
+    public class Pizza
+    {
+
+        public Pizza(int toppingSize, string crust, params string[] toppings)
+        {
+            
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            PizzaId = rnd.Next(0, int.MaxValue);
+
+            Toppings = new string[toppingSize];
+
+            Crust = crust;
+        }
+
+        public Int64 PizzaId;
+        
+        public string[] Toppings = new string[10];
+
+
+        public string Crust;
+
+        public string Sauce;
+
+        public int Size;
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Models;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[10];
+
+            //List<int> myNumber = new List<int>();
+            //myNumber.Add(6);
+            //myNumber.Remove(4);
+
+
+        }
+
+        private static void DisplayPizza(Pizza pizza)
+        {
+            Console.WriteLine(pizza.Crust, pizza.Size, pizza.Sauce);
+        }
+
+        private static void DisplayPizza(string crust, int size, string sauce)
+        {
+            Console.WriteLine("Crust: {0} Size: {1} Sauce {2}", crust, size, sauce);
+        }
+
+    }
+}
